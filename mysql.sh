40ae80eb903b7bc3324eececcdeaeaf1010c3e5d
Alfred <kanalfred@gmail.com>
2017-01-08 04:08:00 -0800

    update comment

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,6 @@
 set -x
 # todo shell: check if the volume path exit and create as need
 # change MYSQL_ROOT_PASSWORD password
+# Set password : export DOCKER_PASS='xxxx'
 DOCKER_PASS=${DOCKER_PASS:='docker!'}
 docker run --name mysql -p 3306:3306 -v /home/alfred/workspace/docker/data/mysql/db:/var/lib/mysql --restart=always -e MYSQL_ROOT_PASSWORD=$DOCKER_PASS -d mysql
--- nginx-update.sh
+++ nginx-update.sh
@@ -1,7 +1,6 @@
 set -x
 # todo shell: check if the volume path exit and create as need
 # change MYSQL_ROOT_PASSWORD password
-# Set password : export DOCKER_PASS='xxxx'
 docker pull kanalfred/nginx
 docker rm -f nginx
 docker run -e 'apple=aboy!' -h nginx --name nginx -p 2200:22 -p 80:80 -p 443:443 -d -v /home/alfred/workspace/docker/data/nginx/data:/data kanalfred/nginx
