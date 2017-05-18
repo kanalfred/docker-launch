be3e23451a1a74be77606f8f5690bb1198ca41a2
Alfred <kanalfred@gmail.com>
2017-05-17 22:23:35 -0700

    Update fileserver.sh to support update.sh

--- fileserver.sh
+++ fileserver.sh
@@ -1,8 +1,14 @@
 set -x
+DIRNAME=$(dirname `readlink -f -- $0`)
+
+# vars require by update.sh
+CONTAINTER_NAME="fileserver"
+IMAGE_TAG="kanalfred/fileserver"
+
 # virutal nic 
 IPADDRESS="192.168.3.130"
-docker run -h fileserver --name fileserver \
+docker run -h fileserver --name $CONTAINTER_NAME \
 --restart=always \
 --volumes-from data-fileserver --volumes-from data-fileserver-bak --volumes-from data-other \
 -p $IPADDRESS:2200:22 -p $IPADDRESS:445:445 -p $IPADDRESS:139:139 -p $IPADDRESS:135:135 -p $IPADDRESS:137:137/udp -p $IPADDRESS138:138/udp \
--d kanalfred/fileserver
+-d $IMAGE_TAG
