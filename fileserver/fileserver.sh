69b2057e0b17ca31e2ab8e0120349a88327bd47e
Alfred <kanalfred@gmail.com>
2017-07-19 19:09:51 -0700

    update fileserver use system unit as service intead of using shell script

--- fileserver/fileserver.sh
+++ fileserver/fileserver.sh
@@ -10,5 +10,5 @@ IPADDRESS="192.168.3.130"
 docker run -h fileserver --name $CONTAINTER_NAME \
 --restart=always \
 --volumes-from data-fileserver --volumes-from data-fileserver-bak --volumes-from data-other \
--p $IPADDRESS:2200:22 -p $IPADDRESS:445:445 -p $IPADDRESS:139:139 -p $IPADDRESS:135:135 -p $IPADDRESS:137:137/udp -p $IPADDRESS138:138/udp \
+-p $IPADDRESS:2200:22 -p $IPADDRESS:445:445 -p $IPADDRESS:139:139 -p $IPADDRESS:135:135 -p $IPADDRESS:137:137/udp -p $IPADDRESS:138:138/udp \
 -d $IMAGE_TAG
